00fb3e8d46c891208f270b9b6646982ed7e4135c
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-11-06 00:26:28 +0100

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'games#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games, only: [ :new, :create, :show ] do
     resources :clues, only: :create
